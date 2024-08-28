e3482ba22f173356b87e05e9e2391ac3433ba309
AndreiDev <a.v.babici@gmail.com>
2024-08-28 20:28:24 +0300

    connect single page to back-end

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Image from '../../../components/Image/Image';
-import './SinglePost.css';
+import Image from '../../../components/Image/Image'
+import './SinglePost.css'
 
 class SinglePost extends Component {
   state = {
@@ -10,16 +10,16 @@ class SinglePost extends Component {
     date: '',
     image: '',
     content: ''
-  };
+  }
 
   componentDidMount() {
-    const postId = this.props.match.params.postId;
-    fetch('URL')
+    const postId = this.props.match.params.postId
+    fetch('http://localhost:8080/feed/post/' + postId)
       .then(res => {
         if (res.status !== 200) {
-          throw new Error('Failed to fetch status');
+          throw new Error('Failed to fetch status')
         }
-        return res.json();
+        return res.json()
       })
       .then(resData => {
         this.setState({
@@ -27,11 +27,11 @@ class SinglePost extends Component {
           author: resData.post.creator.name,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
-        });
+        })
       })
       .catch(err => {
-        console.log(err);
-      });
+        console.log(err)
+      })
   }
 
   render() {
@@ -46,8 +46,8 @@ class SinglePost extends Component {
         </div>
         <p>{this.state.content}</p>
       </section>
-    );
+    )
   }
 }
 
-export default SinglePost;
+export default SinglePost
