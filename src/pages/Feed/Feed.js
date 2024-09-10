2ab27f81fadb20d234da45fabc946b89af736ecd
AndreiDev <a.v.babici@gmail.com>
2024-09-11 00:56:30 +0300

    refactoring image uploading

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react'
-//import openSocket from 'socket.io-client'
 
 import Post from '../../components/Feed/Post/Post'
 import Button from '../../components/Button/Button'
@@ -40,44 +39,7 @@ class Feed extends Component {
       .catch(this.catchError)
 
     this.loadPosts()
-    // const socket = openSocket('http://localhost:8080')
-    // socket.on('posts', data => {
-    //   if (data.action === 'create') {
-    //     this.addPost(data.post)
-    //   } else if (data.action === 'update') {
-    //     this.updatePost(data.post)
-    //   } else if (data.action === 'delete') {
-    //     this.loadPosts()
-    //   }
-    // })
   }
-
-  // addPost = post => {
-  //   this.setState(prevState => {
-  //     const updatedPosts = [...prevState.posts]
-  //     if (prevState.postPage === 1) {
-  //       updatedPosts.pop()
-  //       updatedPosts.unshift(post)
-  //     }
-  //     return {
-  //       posts: updatedPosts,
-  //       totalPosts: prevState.totalPosts + 1
-  //     }
-  //   })
-  // }
-  //
-  // updatePost = post => {
-  //   this.setState(prevState => {
-  //     const updatedPosts = [...prevState.posts]
-  //     const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id)
-  //     if (updatedPostIndex > -1) {
-  //       updatedPosts[updatedPostIndex] = post
-  //     }
-  //     return {
-  //       posts: updatedPosts
-  //     }
-  //   })
-  // }
 
   loadPosts = direction => {
     if (direction) {
@@ -187,35 +149,46 @@ class Feed extends Component {
       editLoading: true
     })
     const formData = new FormData()
-    formData.append('title', postData.title)
-    formData.append('content', postData.content)
     formData.append('image', postData.image)
-
-    let graphqlQuery = {
-      query: `
-        mutation {
-          createPost(postInput: {title: "${postData.title}", content: "${postData.content}", imageUrl: "some url"}) {
-            _id
-            title
-            content
-            imageUrl
-            creator {
-              name
-            }
-            createdAt
-          }
-        }
-      `
+    if (this.state.editPost) {
+      formData.append('oldPath', this.state.editPost.imagePath)
     }
-
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
-      body: JSON.stringify(graphqlQuery),
+    fetch('http://localhost:8080/post-image', {
+      method: 'PUT',
       headers: {
-        Authorization: 'Bearer ' + this.props.token,
-        'Content-Type': 'application/json'
-      }
+        Authorization: 'Bearer ' + this.props.token
+      },
+      body: formData
     })
+      .then(res => res.json())
+      .then(fileResData => {
+        const imageUrl = fileResData.filePath
+        let graphqlQuery = {
+          query: `
+          mutation {
+            createPost(postInput: {title: "${postData.title}", content: "${postData.content}", imageUrl: "${imageUrl}"}) {
+              _id
+              title
+              content
+              imageUrl
+              creator {
+                name
+              }
+              createdAt
+            }
+          }
+        `
+        }
+
+        return fetch('http://localhost:8080/graphql', {
+          method: 'POST',
+          body: JSON.stringify(graphqlQuery),
+          headers: {
+            Authorization: 'Bearer ' + this.props.token,
+            'Content-Type': 'application/json'
+          }
+        })
+      })
       .then(res => {
         return res.json()
       })
@@ -234,7 +207,8 @@ class Feed extends Component {
           title: resData.data.createPost.title,
           content: resData.data.createPost.content,
           creator: resData.data.createPost.creator,
-          createdAt: resData.data.createPost.createdAt
+          createdAt: resData.data.createPost.createdAt,
+          imagePath: resData.data.createPost.imageUrl
         }
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts]
@@ -288,9 +262,9 @@ class Feed extends Component {
         console.log(resData)
         this.loadPosts()
         // this.setState(prevState => {
-        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId)
-        //   return { posts: updatedPosts, postsLoading: false }
-        // })
+        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId);
+        //   return { posts: updatedPosts, postsLoading: false };
+        // });
       })
       .catch(err => {
         console.log(err)
